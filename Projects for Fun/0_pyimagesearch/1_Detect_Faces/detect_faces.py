b573d42c078c01f8b94a7ed7a4fbf6afc8cc7044
anmol94gupta <gupta.anmol103@gmail.com>
2021-03-05 23:38:29 +0530

    pyimagesearch Day1

---
+++ Projects for Fun/0_pyimagesearch/1_Detect_Faces/detect_faces.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  5 23:07:17 2021
+
+@author: anmol
+"""
+
+# import the necessary packages
+import numpy as np
+import argparse
+import cv2
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to input image")
+ap.add_argument("-p", "--prototxt", required=True,
+	help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", required=True,
+	help="path to Caffe pre-trained model")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+	help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
