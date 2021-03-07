9a15a4f9cbd86dff89d12a50a8cf9a5e9d60d1d5
anmol94gupta <gupta.anmol103@gmail.com>
2021-03-08 02:58:58 +0530

    Datect Faces Image & Video

---
+++ Projects for Fun/0_Basics/basics.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  8 00:24:09 2021
+
+@author: anmol
+"""
+
+# IMPORTING PACKAGES
+import imutils
+import cv2
+
+#---------------------
+# LOADING IMAGES
+# load the input image and show its dimensions, keeping in mind that
+# images are represented as a multi-dimensional NumPy array with
+# shape no. rows (height) x no. columns (width) x no. channels (depth)
+image = cv2.imread("1.png")
+(h, w, d) = image.shape
+print("width={}, height={}, depth={}".format(w, h, d))
+# display the image to our screen -- we will need to click the window
+# open by OpenCV and press a key on our keyboard to continue execution
+
+#---------------------
+# PRINTING IMAGES
+cv2.imshow("Image", image)
+cv2.waitKey(0)
+
+#---------------------
+#ACCESSING RGB VALUES
+
+# access the RGB pixel located at x=50, y=100, keepind in mind that
+# OpenCV stores images in BGR order rather than RGB
+(B, G, R) = image[100, 50]
+print("R={}, G={}, B={}".format(R, G, B))
+
+#---------------------
+# REGION OF INTERESTS
+
+# extract a 100x100 pixel square ROI (Region of Interest) from the
+# input image starting at x=320,y=60 at ending at x=420,y=160
+roi = image[60:160, 320:420]
+cv2.imshow("ROI", roi)
+cv2.waitKey(0)
+
+#---------------------
+# RESIZING - ignoring aspect ratio
+# resize the image to 200x200px, ignoring aspect ratio
+resized = cv2.resize(image, (100, 200))
+cv2.imshow("Fixed Resizing", resized)
+cv2.waitKey(0)
+
+#---------------------
+# RESIZING - with aspect ratio
+# fixed resizing and distort aspect ratio so let's resize the width
+# to be 300px but compute the new height based on the aspect ratio
+r = 300.0 / w
+dim = (300, int(h * r))
+resized = cv2.resize(image, dim)
+cv2.imshow("Aspect Ratio Resize", resized)
+cv2.waitKey(0)
+# or can us the following =>
+# manually computing the aspect ratio can be a pain so let's use the
+# imutils library instead
+resized = imutils.resize(image, width=300)
+cv2.imshow("Imutils Resize", resized)
+cv2.waitKey(0)
+
+#---------------------
+# ROTATION - Clipped
+# rotation can also be easily accomplished via imutils with less code
+rotated = imutils.rotate(image, -45)
+cv2.imshow("Imutils Rotation", rotated)
+cv2.waitKey(0)
+#---------------------
+# ROTATION - Non Clipped
+# OpenCV doesn't "care" if our rotated image is clipped after rotation
+# so we can instead use another imutils convenience function to help
+# us out
+rotated = imutils.rotate_bound(image, 45)
+cv2.imshow("Imutils Bound Rotation", rotated)
+cv2.waitKey(0)
+
+#---------------------
+# SMOOTHING 
+# apply a Gaussian blur with a 11x11 kernel to the image to smooth it,
+# useful when reducing high frequency noise
+blurred = cv2.GaussianBlur(image, (11, 11), 0)
+cv2.imshow("Blurred", blurred)
+cv2.waitKey(0)
+
+#---------------------
+# DRAWING ON IMAGE
+# draw a 2px thick red rectangle surrounding the face
+output = image.copy()
+cv2.rectangle(output, (320, 60), (420, 160), (0, 0, 255), 2)
+cv2.imshow("Rectangle", output)
+cv2.waitKey(0)
+
+# draw a blue 20px (filled in) circle on the image centered at
+# x=300,y=150
+output = image.copy()
+cv2.circle(output, (300, 150), 20, (255, 0, 0), -1)
+cv2.imshow("Circle", output)
+cv2.waitKey(0)
+
+# draw a 5px thick red line from x=60,y=20 to x=400,y=200
+output = image.copy()
+cv2.line(output, (60, 20), (400, 200), (0, 0, 255), 5)
+cv2.imshow("Line", output)
+cv2.waitKey(0)
+
+#---------------------
+# WRITE ON IMAGE
+# draw green text on the image
+output = image.copy()
+cv2.putText(output, "OpenCV + Hello Test!!!", (10, 25), 
+	cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2)
+cv2.imshow("Text", output)
+cv2.waitKey(0)
+
+
+
---
+++ Projects for Fun/0_Basics/detect.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  8 00:50:53 2021
+
+@author: anmol
+"""
+
+# IMPORTING PACKAGES
+import argparse
+import imutils
+import cv2
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to input image")
+args = vars(ap.parse_args())
+
+# CONVERTING TO GRAYSCALE
+# load the input image (whose path was supplied via command line
+# argument) and display the image to our screen
+image = cv2.imread(args["image"])
+cv2.imshow("Image", image)
+cv2.waitKey(0)
+# convert the image to grayscale
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow("Gray", gray)
+cv2.waitKey(0)
+
+# EDGE DETECTION
+# applying edge detection we can find the outlines of objects in
+# images
+edged = cv2.Canny(gray, 30, 150)
+cv2.imshow("Edged", edged)
+cv2.waitKey(0)
