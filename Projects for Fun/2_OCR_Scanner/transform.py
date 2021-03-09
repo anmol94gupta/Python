368ddb45487bdfe79b0b12c37e03235f465c46fd
anmol94gupta <gupta.anmol103@gmail.com>
2021-03-09 23:22:09 +0530

    OCR Scanner Updated comments

--- Projects for Fun/2_OCR_Scanner/transform.py
+++ Projects for Fun/2_OCR_Scanner/transform.py
@@ -7,11 +7,13 @@ def order_points(pts):
 	# the second entry is the top-right, the third is the
 	# bottom-right, and the fourth is the bottom-left
 	rect = np.zeros((4, 2), dtype = "float32")
+	
 	# the top-left point will have the smallest sum, whereas
 	# the bottom-right point will have the largest sum
 	s = pts.sum(axis = 1)
 	rect[0] = pts[np.argmin(s)]
 	rect[2] = pts[np.argmax(s)]
+	
 	# now, compute the difference between the points, the
 	# top-right point will have the smallest difference,
 	# whereas the bottom-left will have the largest difference
